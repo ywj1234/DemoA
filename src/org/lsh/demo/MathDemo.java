cdd3b9e46fb7c4980bfde14686a671418c74fe97
xy <xy@192.168.8.20>
2017-02-15 11:33:34 +0800

    new jenkins project b Ywj

---
+++ src/org/lsh/demo/MathDemo.java
@@ -0,0 +1,15 @@
+package org.lsh.demo;
+
+public class MathDemo {
+	public static void main(String[] args) {
+		Math m = new Math();
+		m.div(10, 10);
+
+	}
+}
+
+class Math{
+	public int div(int i,int j){
+		return i/j;
+	}
+}
